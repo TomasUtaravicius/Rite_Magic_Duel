162d000011be4396845510257fa063c4f7acb590
Tomas Utaravicius <tomasutaravicius@gmail.com>
2019-12-05 17:33:36 +0100

    Testing photonTransformViews on vrtk objects.

---
+++ Assets/Scripts/SpellSlotAnimator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellSlotAnimator : MonoBehaviour
+{
+    Animator animator;
+
+    public ParticleSystem ps;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.animator.GetCurrentAnimatorStateInfo(0).IsName("GestureAnimation") && this.animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 0.97f)
+        {
+
+            var em = ps.emission;
+            em.enabled = false;
+
+
+
+
+
+
+        }
+        if (this.animator.GetCurrentAnimatorStateInfo(0).IsName("GestureAnimation") && this.animator.GetCurrentAnimatorStateInfo(0).normalizedTime <= 0.03f)
+        {
+
+            var em = ps.emission;
+            em.enabled = true;
+
+
+
+        }
+
+
+    }
+  
+}
